02dd4eeb28c0a55dbf144df32ea1218cbd48b9cb
unknown <kazue.inada@gmail.com>
2022-03-30 10:19:30 -0300

    componentes criados

--- modulo04/projeto-labex/labex/src/pages/ListTripsPage.js
+++ modulo04/projeto-labex/labex/src/pages/ListTripsPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 
-export default function App() {
+export default function ListTripsPage() {
   return (
     <div className="App">
       
